aef9b4bc09861c0c67a9d7e06ddb21d08c32c8b8
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-07-29 23:30:39 +0800

    add ui

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,6 @@
+import LoginForm from "@/components/loginForm";
+
+
+export default function Login() {
+    return <LoginForm/>
+}
---
+++ src/components/signup-form.tsx
@@ -0,0 +1,5 @@
+export default function SignupForm() {
+    return(
+        <h1>sign up form</h1>
+    )
+}
